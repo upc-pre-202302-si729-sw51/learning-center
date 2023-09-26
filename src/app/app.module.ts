3a2e69f88cc00ab4e167778b64333d1e2633e9d6
Angel Velasquez <avelasquezn@gmail.com>
2023-09-26 07:55:07 -0500

    feat(students): implemented student form layout and visualization characteristics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,13 @@ import {HttpClientModule} from "@angular/common/http";
 import {StudentsComponent} from './learning/pages/students/students.component';
 import {StudentsService} from "./learning/services/students.service";
 import { StudentFormComponent } from './learning/components/student-form/student-form.component';
+import {MatInputModule} from "@angular/material/input";
+import {FormsModule} from "@angular/forms";
+import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
   declarations: [AppComponent, StudentsComponent, StudentFormComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, MatInputModule, FormsModule, MatButtonModule],
   providers: [StudentsService],
   bootstrap: [AppComponent]
 })
