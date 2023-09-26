3c37b60a09626a15bdf806c9f8d7e92172b7caa4
Angel Velasquez <avelasquezn@gmail.com>
2023-09-26 08:55:22 -0500

    feat(page-not-found): implemented behavior.

--- src/app/public/pages/page-not-found/page-not-found.component.ts
+++ src/app/public/pages/page-not-found/page-not-found.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-page-not-found',
   templateUrl: './page-not-found.component.html',
   styleUrls: ['./page-not-found.component.css']
 })
-export class PageNotFoundComponent {
+export class PageNotFoundComponent implements OnInit {
+  invalidUrl: string;
+
+  constructor(private route: ActivatedRoute, private router: Router) {
+    this.invalidUrl = '';
+  }
+
+  // UI Event Handlers
+
+  onNavigateHome() {
+    this.router.navigate(['home']).then();
+  }
+
+  // Component Lifecycle Event Handlers
+  ngOnInit(): void {
+    this.invalidUrl = this.route.snapshot.url[0].path;
+  }
+
+
 
 }
